1e2d0128b12e9bd6f0a5923b6c659b239fab8627
Zeshan Tawa <zeshan.tawa98@gmail.com>
2023-02-23 19:26:49 +0000

    Complete boolean operator assignment

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -14,7 +14,7 @@ public class Lab {
      * @return if b is true, return true. if b is false, return false.
      */
     public boolean returnBoolean(boolean b){
-        return false;
+        return b;
     }
 
     /**
@@ -23,7 +23,7 @@ public class Lab {
      * look into boolean operations to do this without using if/else statements.
      */
     public boolean returnNot(boolean b){
-        return false;
+        return !b;
     }
 
     /**
@@ -33,7 +33,7 @@ public class Lab {
      * look into boolean operations to do this without using if/else statements.
      */
     public boolean returnAnd(boolean b1, boolean b2){
-        return false;
+        return (b1 & b2);
     }
 
     /**
@@ -43,7 +43,7 @@ public class Lab {
      * look into boolean operations to do this without using if/else statements.
      */
     public boolean returnOr(boolean b1, boolean b2){
-        return false;
+        return (b1 | b2);
     }
 
 
